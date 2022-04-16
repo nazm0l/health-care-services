f6221c3115f4bbdcb767143dc797b31eb71c49a8
nazm0l <tanjirahmednazmul@gmail.com>
2022-04-17 05:42:10 +0600

    getting form value successfully

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleLogin = event =>{
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password)
+    }
+
   return (
     <div className="container">
       <h2 className="text-center mt-5">Login Here</h2>
-      <div className="w-25 mx-auto">
-        <Form>
+      <div className="w-50 mx-auto">
+        <Form onSubmit={handleLogin}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
           </Form.Group>
           <Button variant="dark" type="submit">
             LogIn
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleRegister = event =>{
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(name, email, password);
+    }
+
   return (
     <div className="container">
       <h2 className="text-center mt-5">Register Here</h2>
-      <div className="w-25 mx-auto">
-        <Form>
+      <div className="w-50 mx-auto">
+        <Form onSubmit={handleRegister}>
           <Form.Group className="mb-3" controlId="formBasicText">
             <Form.Label>Full name</Form.Label>
-            <Form.Control type="text" placeholder="Your name" />
+            <Form.Control ref={nameRef} type="text" placeholder="Your name" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
           </Form.Group>
           <Button variant="dark" type="submit">
             Register
