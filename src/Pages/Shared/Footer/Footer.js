87375b82e2b4ecdc2e2271e6373955546345551f
nazm0l <tanjirahmednazmul@gmail.com>
2022-04-17 03:50:08 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+    const date = new Date();
+    const year = date.getFullYear()
+
+  return (
+    <div>
+      <footer className="text-center mt-5 text-white bg-dark py-3">
+        <p>
+          <small>&copy;{year} - Health Care Services. All Rights Reserved.</small>
+        </p>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
