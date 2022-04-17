bf11440f3e5edb157d24b1b287b77da6c1dd80c1
nazm0l <tanjirahmednazmul@gmail.com>
2022-04-17 07:03:06 +0600

    google auth added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,32 +1,34 @@
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { Link, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
 
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const navigate = useNavigate();
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
 
-    const [
-        signInWithEmailAndPassword,
-        user,
-        loading,
-        error,
-      ] = useSignInWithEmailAndPassword(auth);
+    const [signInWithGoogle, userGoogle, loadingGoogle, errorGoogle] = useSignInWithGoogle(auth);
 
-    if(user){
-        navigate('/')
-    }
+  if (user || userGoogle) {
+    navigate("/");
+  }
 
-    const handleLogin = event =>{
-        event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        signInWithEmailAndPassword(email, password);
-    }
+  let errorMessage;
+    if (error || errorGoogle) {
+        errorMessage = <p className='text-danger'>{error?.message} {errorGoogle.message}</p>
+      }
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
+  };
 
   return (
     <div className="container">
@@ -35,18 +37,43 @@ const Login = () => {
         <Form onSubmit={handleLogin}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+            <Form.Control
+              ref={emailRef}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+            <Form.Control
+              ref={passwordRef}
+              type="password"
+              placeholder="Password"
+              required
+            />
           </Form.Group>
           <Button variant="dark" type="submit">
             LogIn
           </Button>
         </Form>
-        <p className="mt-2">New Here?<Link className="text-decoration-none ps-2" to="/register">Register Now</Link></p>
+        <p className="mt-2">
+          New Here?
+          <Link className="text-decoration-none ps-2" to="/register">
+            Register Now
+          </Link>
+        </p>
+        {errorMessage}
+        {/*------------ Social Login --------- */}
+        <div className="d-flex align-items-center">
+          <div style={{ height: "1px" }} className="bg-dark w-50"></div>
+          <p className="mt-2 px-2">OR</p>
+          <div style={{ height: "1px" }} className="bg-dark w-50"></div>
+        </div>
+        <div className="text-center">
+            <button onClick={()=>signInWithGoogle()} className="btn btn-dark">Continue with Google</button>
+        </div>
       </div>
     </div>
   );
